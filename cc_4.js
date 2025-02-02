95db717234490f34a2dbb2321466e1e08e6502cb
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-01 21:21:03 -0500

    Task2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -1,8 +1,16 @@
 // Task 1 - If Statements
-let purchaseAmount = 500; // Purchase amount.
+let purchaseAmount = 500; // Declaring variable with value.
 let finalAmount = purchaseAmount; // Final amount for discount.
 if (purchaseAmount > 100) {
     finalAmount = purchaseAmount * 0.85; // 15% discount.
     console.log("Discount applied")
 };
 console.log(`Final amount after discount: $${finalAmount}`); // Logging final amount to console.
+
+// Task 2 - For Loop
+let sales = [120, 85, 200, 150, 90]; // Declaring an array with five sales figures.
+let totalSales = 0;
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i]
+}; // Using for loop to calculate total sales.
+console.log(`Total Sales: $${totalSales}`); // Logging total sales to console.
